1bdf93075e0edb613e60de3c5c68b6dc0da386a8
Cristiano Bento <crisdb2008@gmail.com>
2022-09-10 13:05:57 -0300

    react1

---
+++ src/components/Form.js
@@ -0,0 +1,26 @@
+function Form(){
+
+function cadastrarUsuario(e){
+ e.preventDefault()
+    console.log("Cadastrou")
+}
+
+return(
+<div>
+    <h1> Meu Cadastro</h1>
+    <form onSubmit={cadastrarUsuario}>
+        <div>
+            <input type="text" placeholder = "Digite seu nome"/>
+        </div>
+        <div>
+            <input type="submit" value="Cadastrar"/>
+        </div>
+    </form>
+</div>
+
+)
+
+
+}
+
+export default Form
